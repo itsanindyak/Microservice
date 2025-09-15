38c493ba5af9aa6a2a82096b0e007ec34b121b1f
itsanindyak <iankoley04@gmail.com>
2025-09-15 23:54:44 +0530

    remove password field from req.user in  auth middleware

--- user/middleware/auth.middleware.js
+++ user/middleware/auth.middleware.js
@@ -18,11 +18,12 @@ export const userAuth = async (req, res, next) => {
 
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
-    const user = await userModel.findById(decoded.id);
+    const user = await userModel.findById(decoded.id).select("-password");
 
     if (!user) {
       return res.status(401).json({ message: "Unauthorized" });
     }
+  
 
     req.user = user;
 
