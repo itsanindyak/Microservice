4052500dcec77b9130a840810f2add7dc812cfdd
itsanindyak <iankoley04@gmail.com>
2025-09-17 20:05:21 +0530

    refactor the queue object creation in user service

--- user/service/messageQueue.js
+++ user/service/messageQueue.js
@@ -1,13 +1,11 @@
 import RabbitMQ from "./rabbit.js";
-let instance;
 
-export function getRabbit() {
-  if (!instance) instance = new RabbitMQ();
-  return instance;
-}
+const rabbit = new RabbitMQ();
 
 export async function initRabbit() {
-  const rabbit = new RabbitMQ();
   rabbit.setUrl(process.env.RABBIT_MQ_URL);
   await rabbit.connect();
 }
+
+
+export default rabbit;
