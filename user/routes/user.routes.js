c42ea9b986d07b2140254f07e3fe897970396104
itsanindyak <iankoley04@gmail.com>
2025-09-17 09:39:59 +0530

    add health check route in user server

--- user/routes/user.routes.js
+++ user/routes/user.routes.js
@@ -1,11 +1,22 @@
 import { Router } from "express";
-import { login, logout, profile, register } from "../controller/user.controller.js";
+import {
+  login,
+  logout,
+  profile,
+  register,
+} from "../controller/user.controller.js";
 import { userAuth } from "../middleware/auth.middleware.js";
 
 export const userRoute = Router();
 
+userRoute.get("/", (req, res) => {
+  res.status(200).json({
+    status:"ok",
+    message: "user route",
+  });
+});
 
-userRoute.post("/register",register)
+userRoute.post("/register", register);
 userRoute.post("/login", login);
 userRoute.get("/logout", logout);
 userRoute.get("/profile", userAuth, profile);
