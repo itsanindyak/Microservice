9a94256a710c612b1725fc367482e78a1c9a0bc2
itsanindyak <iankoley04@gmail.com>
2025-09-17 10:16:34 +0530

    add proxy of captain service

--- gateway/app.js
+++ gateway/app.js
@@ -5,6 +5,7 @@ import expressProxy from "express-http-proxy";
 const app = express();
 
 app.use("/user",expressProxy("http://localhost:3001"));
+app.use("/captain",expressProxy("http://localhost:4001"))
 
 app.listen(3000, () => {
     console.log("Backend server is running on port 3000");
