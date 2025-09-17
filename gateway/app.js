3ac0b64f51eaa47341e738e5d0f73756177bd0d8
itsanindyak <iankoley04@gmail.com>
2025-09-17 19:57:59 +0530

    add morgan for clear log

--- gateway/app.js
+++ gateway/app.js
@@ -1,12 +1,14 @@
 import express from "express";
 import expressProxy from "express-http-proxy";
-
+import morgan from "morgan";
 
 const app = express();
+app.use(morgan("dev"));
 
-app.use("/user",expressProxy("http://localhost:3001"));
-app.use("/captain",expressProxy("http://localhost:4001"))
+app.use("/user", expressProxy("http://localhost:3001"));
+app.use("/captain", expressProxy("http://localhost:4001"));
+app.use("/ride", expressProxy("http://localhost:5001"));
 
 app.listen(3000, () => {
-    console.log("Backend server is running on port 3000");
+  console.log("Backend server is running on port 3000");
 });
